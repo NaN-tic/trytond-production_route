170d899b883ecb6c90b74c80a7a636fe15dd43cd
Albert Cervera i Areny <albert@nan-tic.com>
2014-04-10 00:41:09 +0200

    If route is removed, field in product should be set null.

--- product.py
+++ product.py
@@ -10,7 +10,7 @@ class ProductBom(ModelSQL, ModelView):
     __name__ = 'product.product-production.bom'
 
     # TODO: Add domain filter
-    route = fields.Many2One('production.route', 'Route')
+    route = fields.Many2One('production.route', 'Route', ondelete='SET NULL')
         #domain=[
         #    ('uom', '=', Get(Eval('_parent_product', {}), 'default_uom', 0)),
         #    ], depends=['product']
